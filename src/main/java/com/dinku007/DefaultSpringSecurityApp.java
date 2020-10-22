824f82f25d1a2d1f353e5915b30bc9f1e26c392d
Anand <anamastemath@obsessory.com>
2020-10-22 19:02:25 +0530

    Initial Commit

---
+++ src/main/java/com/dinku007/DefaultSpringSecurityApp.java
@@ -0,0 +1,14 @@
+package com.dinku007;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Anand SN
+ */
+
+@SpringBootApplication
+public class DefaultSpringSecurityApp {
+    public static void main(String[] args) {
+        SpringApplication.run(DefaultSpringSecurityApp.class,args);
+    }
+}
